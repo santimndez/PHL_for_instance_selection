6da901f7097245404bb9fc537d5dbe5d83743467
Santiago <gemelosmendez@gmail.com>
2025-06-23 04:32:02 +0200

    experiment analysis notebook. Faltan los resultados de diabetes binary

--- src/my_dataset_reduction/imblearn_wrapper.py
+++ src/my_dataset_reduction/imblearn_wrapper.py
@@ -7,26 +7,6 @@ from imblearn.under_sampling import RandomUnderSampler
 from imblearn.under_sampling import CondensedNearestNeighbour
 
 from sklearn.cluster import KMeans
-##############################################################################
-#CLC
-#Clustering Centroids Selection
-
-# def clc_selection_toscano(X,y,perc):
-#     
-#     classes = np.unique(y)
-#     X_res = np.array([],dtype=float)
-#     y_res = np.array([],dtype=float)
-#     n_features = np.shape(X)[1]
-#     
-#     for cl in classes:
-#         pool_cl = np.where(y==cl)
-#         X_cl = X[pool_cl]
-#         n_cl = trunc(len(X_cl)*perc)
-#         centroids = kmeans(X_cl,k_or_guess=n_cl,iter=1)[0]
-#         X_res = np.append(X_res, centroids)
-#         y_res = np.append(y_res, np.repeat(cl,len(centroids)))
-#         
-#     return X_res.reshape(-1, n_features), y_res
 
 def clc_selection(X, y, perc):
     # Number of selected instances per class
